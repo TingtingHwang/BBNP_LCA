677fe03286f4c5ec29e7f9e5a2ba0dfa0f50a0bd
TingtingHwang <bjyx13529@gmail.com>
2025-01-27 17:52:20 +0800

    upload

--- 2_Clustering/SOFM/Dataset/README.md
+++
@@ -1,5 +0,0 @@
-# Dataset
-'Caltech101-20' datasets is in tag.
-Please run the 'demo.m', and reproduce the results on 'Caltech101-20' datasets.
-
-
--- 6_CrossValidation/lib/mynorm.m
+++
@@ -1,9 +0,0 @@
-function results = mynorm(image,h,w,mask)
-
-image=single(image).*single(mask);
-
-MaxV=max(image(mask==1));
-MinV=min(image(mask==1));
-results=(image-MinV)/(MaxV-MinV);
-results=reshape(results,h*w,1);
-end
